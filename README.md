acdad3ff10554e40fa86235e21fac9669392881d
mike-carbone <mfcbone@gmail.com>
2018-07-31 10:46:15 -0400

    :memo: Update readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# idm250
+# DRIP
+### Project for IDM250
+
+---
+
+This is going to be a tech blog/ blog about anything I want to write about. Cool things I see or learn, things that are up and coming, whatever. It's essentially a personal blog with a brand behind it.
--- wp-content/themes/idm250/functions.php
+++ wp-content/themes/idm250/functions.php
@@ -1,4 +1,31 @@
 <?php
-	echo "this displays first";
-	echo "<br>";
-?>
+
+// Theme will only work for WordPress 4.9.6 or later.
+if (version_compare($GLOBALS['wp_version'], '4.9.6', '<')) {
+    die('This theme only works in WordPress 4.9.6 or later. ');
+}
+
+/**
+ * This function will register the navigation menus.
+ * You can call display the menu by calling wp_nav_menu() in your templates
+ *
+ * @link https://developer.wordpress.org/reference/functions/wp_nav_menu/
+ * @method register_nav_menus
+ */
+function register_menus()
+{
+    register_nav_menus([
+        'main_menu'   => __('Main Navigation'),
+        'footer_menu' => __('Footer Navigation'),
+    ]);
+}
+
+add_action('init', 'register_menus');
+
+function load_theme_assets(){
+	//name, where the file is located, 
+	wp_enqueue_style('bootstrap', get_template_directory_uri() . '/dist/css/bootstrap.css');
+
+	wp_enqueue_script('smooth', get_template_directory_uri() . '/dist/js/SmoothScroll.js');
+}
+add_action('wp_enqueue_scripts', 'load_theme_assets');
